59f893b4d3f974732a7f818a9b7695f2a5175e5b
AidenPearce02 <coolboyvitalij@gmail.com>
2019-04-20 23:29:56 +0200

    work with pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IntroComponent } from './components/pages/intro/intro.component';
+import { FeedComponent } from './components/pages/feed/feed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: IntroComponent},
+  {path: 'feed', component: FeedComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layouts/header/header.component';
 import { FooterComponent } from './components/layouts/footer/footer.component';
+import { IntroComponent } from './components/pages/intro/intro.component';
+import { FeedComponent } from './components/pages/feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IntroComponent,
+    FeedComponent
   ],
   imports: [
     BrowserModule,
